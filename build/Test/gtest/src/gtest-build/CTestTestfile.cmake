e6a3cce3bc7e2c83c50a87c90ce2e10a7d1b9e53
victorhoe <hky.victor@gmail.com>
2017-08-04 20:46:19 -0400

    Add build folder to gitignore

--- build/Test/gtest/src/gtest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/victor/Documents/Source/C++/MatrixSolver/build/Test/gtest/src/gtest
-# Build directory: /home/victor/Documents/Source/C++/MatrixSolver/build/Test/gtest/src/gtest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
--- build/Test/gtest/src/gtest-stamp/verify-gtest.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/victor/Documents/Source/C++/MatrixSolver/build/Test/gtest/src/master.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
